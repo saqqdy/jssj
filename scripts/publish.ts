74ec97029ffc3debe7fb602605650038ab5afac6
saqqdy <saqqdy@qq.com>
2022-05-03 19:46:00 +0800

    update packages

--- scripts/publish.ts
+++ scripts/publish.ts
@@ -1,20 +1,26 @@
 import { execSync } from 'child_process'
-import path from 'path'
+import { join } from 'path'
 import consola from 'consola'
 import { version } from '../package.json'
 import { packages } from '../build/packages'
 
-// execSync('npm run build', { stdio: 'inherit' })
+// execSync('pnpm build', { stdio: 'inherit' })
 
-let command = 'npm publish --access public'
+const REGISTRY_URL = 'https://registry.npmjs.org'
+let command = `npm --registry=${REGISTRY_URL} publish --access public`
 
+if (version.includes('rc')) command += ' --tag release'
 if (version.includes('beta')) command += ' --tag beta'
 if (version.includes('alpha')) command += ' --tag alpha'
 
-for (const { name } of packages) {
+for (const { pkgName, name } of packages) {
     execSync(command, {
         stdio: 'inherit',
-        cwd: path.join('packages', name, 'dist')
+        cwd: join('packages', name, 'dist')
     })
-    consola.success(`Published @jssj/${name}`)
+    consola.success(`Published ${pkgName}`)
 }
+execSync(command, {
+    stdio: 'inherit'
+})
+consola.success('Published @jssj/monorepo')

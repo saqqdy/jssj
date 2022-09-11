c736abea6491b570b3fc4f107394b04495770a2b
saqqdy <saqqdy@qq.com>
2022-09-11 11:07:43 +0800

    update packages

--- scripts/reinstall.ts
+++ scripts/reinstall.ts
@@ -17,7 +17,7 @@ pkg = JSON.parse(pkg)
 
 const useWorkspace = true // 是否启用了workspace模式
 const PACKAGE_NEXT: string[] = [] // 需要安装next版本的包
-const PACKAGE_EXCLUDE: string[] = [] // 忽略的包
+const PACKAGE_EXCLUDE: string[] = ['@rollup/plugin-node-resolve'] // 忽略的包
 const PACKAGE_MANAGERS: TypeManagers[] = ['pnpm', 'yarn', 'npm'] // 包管理工具优先级
 const cmd = getPackageManager()
 
